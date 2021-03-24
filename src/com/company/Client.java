b951b4c08efbc64f9d18d3f6c9df6e9d41adc33e
Daniel Coleman <dccol@student.unimelb.edu.au>
2021-03-24 18:14:59 +1100

    Client template

---
+++ src/com/company/Client.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    // IP and port
+    private static String serverIp;
+    private static int serverPort;
+
+    public static void main(String[] args)
+    {
+        // READ IN COMMAND LINE ARGS
+        if(args.length < 2){
+            System.out.println("Missing Arguments. Please Try Again");
+        }
+        else if(args.length > 2){
+            System.out.println("Too many Arguments. Please Try Again");
+        }
+        serverIp = args[0];
+        serverPort = Integer.parseInt(args[1]);
+
+        try(Socket socket = new Socket(serverIp, serverPort);)
+        {
+            // Output and Input Stream
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            String sendData ="I want to connect";
+            output.writeUTF(sendData);
+            System.out.println("Data sent to Server--> " + sendData);
+            output.flush();
+
+            while(true && input.available() > 0)
+            {
+                String message = input.readUTF();
+                System.out.println(message);
+            }
+
+        }
+        catch (UnknownHostException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
